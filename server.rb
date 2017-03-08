ba5cb2ad8bcf681cf5c0266a364998d75934436c
chand1012 <chandlerl14@outlook.com>
2017-03-07 22:25:16 -0500

    got basic code working

--- server.rb
+++ server.rb
@@ -1,19 +1,28 @@
 require 'socket'
 require 'open3'
 
-hostname = '10.34.137.141'
+hostname = '192.168.1.7'
 port = 2000
 
 s = TCPServer.open(hostname, port)
 
-while line = s.gets   # Read lines from the socket
-  s.print("#{Dir.pwd}$")
-  if line.chop == 'exit'
-    s.close
-    exit
-  else
-    stdin, stout, status = Open3.capture3(line.chop)      # And print with platform line terminator
-    s.puts(stout)
+loop do
+  client = s.accept
+  while line = client.gets   # Read lines from the socket
+    if line.chop == 'exit'
+      client.close
+      exit
+    else
+      begin
+        stdin, stout, status = Open3.capture3(line.chop)      # And print with platform line terminator
+        client.puts(stdin)
+        puts(stdin)
+      rescue
+        client.puts("command not found")
+      end
+    end
   end
+  client.close
+
+
 end
-s.close
